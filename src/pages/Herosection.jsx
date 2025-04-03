44a514f521d453e73e3aff662740f9fa31488884
Aayush stha <sudipk339@gmail.com>
2025-04-03 12:26:04 +0545

    DAY 3'

---
+++ src/pages/Herosection.jsx
@@ -0,0 +1,13 @@
+
+import style from "./herosection.module.css";
+export default function Herosection() {
+  return (
+    <div>
+      <p className={style.heroSecHeading}>HeroSection</p>
+      <span className={style.heroSecSubHeading}> This is our Super react app</span>
+    </div>
+  );
+}
+
+
+
